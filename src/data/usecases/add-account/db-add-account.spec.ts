ae5351efc10c1b97d3d475f3cd89648aec6f39b9
Marcos R. Silva <w271592m@motorola.com>
2023-04-11 11:54:56 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -82,4 +82,16 @@ describe("DbAddAccount Usecase", () => {
             password: 'hashed_password'
         });
     });
+
+    it("Should throw if addAccountRepository throws", async () => {
+        const {sut, addAccountRepositoryStub} = makeSut();
+        jest.spyOn(addAccountRepositoryStub,'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
+        const accountData = {
+            name: '',
+            email: '',
+            password: 'validPassword'
+        }
+        const promise = sut.add(accountData);
+        await expect(promise).rejects.toThrow();
+    });
 });
