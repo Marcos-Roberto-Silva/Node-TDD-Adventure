499c9fa9d0961b81281f0c955485d636b5e8df16
Marcos R. Silva <w271592m@motorola.com>
2023-04-28 16:43:48 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,10 +1,9 @@
 import request from 'supertest';
 import app from '../config/app';
 
-describe("", () => {
+describe("Body Parser Middleware", () => {
     it("Should body as jason", async () => {
         app.post('/test_body_parser', (req, res) => {
-            console.log('RESULT: ',req.body)
             res.send(req.body)
         });
         await request(app)
